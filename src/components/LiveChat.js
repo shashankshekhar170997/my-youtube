4ccb7498d2d253154bddfeab24aa4456fd1d9d9b
Shashank <shashankshekhar170997@gmail.com>
2024-02-06 11:12:18 +0530

    Created Live chat with random data && Caching

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../Redux/chatSlice";
+import { generateRamdomName, randomMessage } from "../utils/helper";
 
 const LiveChat = () => {
+  const [LiveMessage, setLiveMessage] = useState("");
   const dispatch = useDispatch();
   //   reading from the store or Subscribing to the store (store.name.initialstate)
   const chatMessages = useSelector((store) => store.chat.messages);
@@ -12,20 +14,46 @@ const LiveChat = () => {
       //   console.log("Api Polling");
       dispatch(
         addMessage({
-          name: "Akshay Shekhar",
-          message: "Lorem ipsum react jsssss",
+          name: generateRamdomName(),
+          message: randomMessage(30),
         })
       );
-    }, 2000);
+    }, 1000);
     // garbage collection (unmount)
     return () => clearInterval(i);
   }, []);
   return (
-    <div className="ml-2 p-2 border border-black w-full h-[500px] bg-slate-100 rounded-lg overflow-y-scroll">
-      {chatMessages.map((c, index) => (
-        <ChatMessage key={index} name={c.name} message={c.message} />
-      ))}
-    </div>
+    <>
+      <div className="ml-2 p-2 border border-black w-full h-[500px] bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+        <div>
+          {chatMessages.map((c, index) => (
+            <ChatMessage key={index} name={c.name} message={c.message} />
+          ))}
+        </div>
+      </div>
+      <form
+        className="m-full p-2 ml-2 border border-black"
+        onSubmit={(e) => {
+          console.log("Submit", LiveMessage);
+          e.preventDefault();
+          dispatch(
+            addMessage({
+              name: "Shashank",
+              message: LiveMessage,
+            })
+          );
+          setLiveMessage("");
+        }}
+      >
+        <input
+          type="text"
+          className=" px-2 w-80"
+          value={LiveMessage}
+          onChange={(e) => setLiveMessage(e.target.value)}
+        />
+        <button className="mx-2 bg-green-500 rounded-sm">Send</button>
+      </form>
+    </>
   );
 };
 
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -182,3 +182,16 @@ export function generateRamdomName() {
   return nameList[Math.floor(Math.random() * nameList.length)];
   //   document.getElementById("uNameInput").value = finalName;
 }
+
+export function randomMessage(length) {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
